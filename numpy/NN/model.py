dc918533bbdd318590b0e550d2e5ad6447bfc256
Christian Johnson <christian.johnson@deepsight.de>
2021-05-16 16:19:11 +0200

    remove lean and blackify model

--- numpy/NN/model.py
+++ numpy/NN/model.py
@@ -1,15 +1,15 @@
 import numpy as np
-from tqdm import tqdm
 import random
 import matplotlib.pyplot as plt
 
 
-class Model():
-    '''
+class Model:
+    """
     Basic, configurable feed-forward model.
-    '''
-    def __init__(self, layers:list=[2, 2]):
-        '''
+    """
+
+    def __init__(self, layers: list = [2, 2]):
+        """
         Initialize network from given
         parameters
 
@@ -18,7 +18,7 @@ class Model():
             layers : list
                 List of integers specifying the
                 number of nodes in each layer.
-        '''
+        """
         # Initialize layer shapes.
         self.layers = layers
         self.n_layers = len(layers)
@@ -28,7 +28,7 @@ class Model():
         self.weights = []
 
     def forward(self, batch):
-        '''
+        """
         Pass a batch of inputs forward
         through the network.
 
@@ -43,7 +43,7 @@ class Model():
                 List of activations,
                 nd.arrays, from each
                 layer of the network.
-        '''
+        """
         # We need to keep the input so we can
         # compute backprop later.
         activations = [np.array(batch)]
@@ -59,7 +59,7 @@ class Model():
 
     @staticmethod
     def sigma(drive):
-        '''
+        """
         Return activation from
         a given drive.
 
@@ -75,14 +75,14 @@ class Model():
         ---------
             activation : np.array
                 Activation values.
-        '''
+        """
         # Avoid np overflow.
         drive = np.clip(drive, -500, 500)
 
         return 1 / (1 + np.exp(-drive))
 
     def sigma_prime(self, drive):
-        '''
+        """
         Derivative of sigma activation
         function.
 
@@ -91,11 +91,11 @@ class Model():
             drive : np.array
                 Derivative activation
                 value given an incoming drive.
-        '''
+        """
         return self.sigma(drive) * (1 - self.sigma(drive))
 
     def back(self, activations, drives, labels):
-        '''
+        """
         Pass updates back
         through the network.
 
@@ -119,7 +119,7 @@ class Model():
                 Update to biases.
             loss : float
                 Update on loss for batch.
-        '''
+        """
         # Instantiate updates.
         delta_w = []
         delta_b = []
@@ -139,10 +139,12 @@ class Model():
 
             else:
                 # Other layers, infer delta via backprop.
-                delta = np.dot(delta, weights_reversed[i-1].T) * self.sigma_prime(d)
+                delta = np.dot(
+                    delta, weights_reversed[i - 1].T
+                ) * self.sigma_prime(d)
 
             db = delta
-            dw = np.dot(activations_reversed[i+1].T, delta)
+            dw = np.dot(activations_reversed[i + 1].T, delta)
             # Apply the delta to the update matrix.
             delta_w.append(dw)
             delta_b.append(db)
@@ -150,14 +152,9 @@ class Model():
         return reversed(delta_w), reversed(delta_b), loss
 
     def fit(
-        self,
-        x,
-        y,
-        epochs:int=10,
-        batch_size:int=100,
-        lr:float=0.01
+        self, x, y, epochs: int = 10, batch_size: int = 100, lr: float = 0.01
     ):
-        '''
+        """
         Fits the network to a set of
         training data.
 
@@ -174,35 +171,44 @@ class Model():
                 samples in each batch.
             lr : float
                 Learning rate.
-        '''
+        """
         # Store batch size for eval.
         self.batch_size = batch_size
         # Flatten all input arrays.
         X = x.reshape(x.shape[0], -1)
         # Add appropriate layers based on input shape.
-        self.weights.extend([np.random.randn(X.shape[1], s) \
-                                if i == 0 else (np.random.randn(self.layers[i-1], s)) \
-                                for i, s in enumerate(self.layers)])
+        self.weights.extend(
+            [
+                np.random.randn(X.shape[1], s)
+                if i == 0
+                else (np.random.randn(self.layers[i - 1], s))
+                for i, s in enumerate(self.layers)
+            ]
+        )
         # Add biases for the weights.
-        self.biases.extend([np.random.randn(1, w.shape[1]) \
-                                for w in self.weights])
+        self.biases.extend(
+            [np.random.randn(1, w.shape[1]) for w in self.weights]
+        )
         # One-hot encoding.
         self.labels = np.eye(np.unique(y).shape[0])
         y = [self.labels[value] for value in y]
         # Add output layer.
-        self.weights.append(np.random.randn(self.weights[-1].shape[1], self.labels.shape[0]))
+        self.weights.append(
+            np.random.randn(self.weights[-1].shape[1], self.labels.shape[0])
+        )
         self.biases.append(np.random.randn(1, self.labels.shape[0]))
         # Organize data for training.
         data = list(zip(X, y))
         n = len(data)
         # Training loop.
         losses = []
-        for i in tqdm(range(epochs)):
+        for i in range(epochs):
             # Shuffle.
             random.shuffle(data)
             # Get batches.
-            batches = [data[k:k+batch_size] \
-                        for k in range(0, n, batch_size)]
+            batches = [
+                data[k : k + batch_size] for k in range(0, n, batch_size)
+            ]
             for b in batches:
                 # Unzip the batch.
                 batch, labels = zip(*b)
@@ -225,8 +231,10 @@ class Model():
                 # Store loss info.
                 losses.append(loss)
 
+            print(i, end=" ")
+
     def eval(self, x, y):
-        '''
+        """
         Evaluate model accuracy
         against test data.
 
@@ -241,7 +249,7 @@ class Model():
         ---------
             acc : float
                 Test accuracy.
-        '''
+        """
         # One-hot encode the labels.
         y = [self.labels[value] for value in y]
         # Flatten all input arrays.
@@ -250,8 +258,9 @@ class Model():
         data = list(zip(X, y))
         n = len(data)
         # Get batches.
-        batches = [data[k:k+self.batch_size] \
-                    for k in range(0, n, self.batch_size)]
+        batches = [
+            data[k : k + self.batch_size] for k in range(0, n, self.batch_size)
+        ]
         acc = []
         # Store info on the predictions.
         preds = []
@@ -276,9 +285,13 @@ class Model():
                 pass
 
         # Visualize samples with a low confidence.
-        results = sorted(list(zip(preds, ims)), key=lambda x: x[0], reverse=False)
+        results = sorted(
+            list(zip(preds, ims)), key=lambda x: x[0], reverse=False
+        )
         sample = np.array([i[1] for i in results[:16]])
-        result = sample.reshape(4, 4, 28, 28).swapaxes(1, 2).reshape(28*4, 28*4)
+        result = (
+            sample.reshape(4, 4, 28, 28).swapaxes(1, 2).reshape(28 * 4, 28 * 4)
+        )
         plt.imshow(result)
         plt.show()
 
--- numpy/NN/train.py
+++ numpy/NN/train.py
@@ -1,24 +1,8 @@
 from keras.datasets import mnist
-from lean import train, evaluate
 from model import Model
 
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-nn = train(
-        [30, 60, 60, 30],
-        x_train,
-        y_train,
-        epochs=10,
-        batch_size=40,
-        lr=0.003
-    )
-print(evaluate(
-        nn,
-        x_test,
-        y_test,
-        40,
-    ))
-# nn = Model([30, 60, 60, 30])
-# nn.fit(x_train, y_train, epochs=50, batch_size=40, lr=0.003)
-# print()
-# print(nn.eval(x_test, y_test))
+nn = Model([30, 60, 60, 30])
+nn.fit(x_train, y_train, epochs=50, batch_size=40, lr=0.003)
+print(nn.eval(x_test, y_test))
