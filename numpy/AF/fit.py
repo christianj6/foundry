2cf6ecc5650a92002af372fb8c31b544f95a047a
Christian Johnson <christian.johnson@deepsight.de>
2021-08-09 15:59:37 +0200

    extract visualization functionality to utility function

--- numpy/AF/fit.py
+++ numpy/AF/fit.py
@@ -1,7 +1,5 @@
 from affinity_propagation import AffinityPropagation
-from tools import generate_random_cluster_data
-import matplotlib.pyplot as plt
-from itertools import cycle
+from tools import generate_random_cluster_data, visualize_clusters
 
 x = generate_random_cluster_data()
 af = AffinityPropagation(
@@ -10,26 +8,4 @@ af = AffinityPropagation(
     plateau=50,
 )
 af.fit(x)
-
-print(len(af.exemplars_))
-colors = dict(zip(af.exemplars_, cycle("bgrcmyk")))
-for i in range(len(af.labels_)):
-    X = x[i][0]
-    Y = x[i][1]
-
-    if i in af.exemplars_:
-        exemplar = i
-        edge = "k"
-        ms = 10
-
-    else:
-        exemplar = af.labels_[i]
-        ms = 3
-        edge = None
-        plt.plot([X, x[exemplar][0]], [Y, x[exemplar][1]], c=colors[exemplar])
-
-    plt.plot(
-        X, Y, "o", markersize=ms, markeredgecolor=edge, c=colors[exemplar]
-    )
-
-plt.savefig("plot.png")
+visualize_clusters(af, x)
--- numpy/AF/tools.py
+++ numpy/AF/tools.py
@@ -1,4 +1,7 @@
 import numpy as np
+from affinity_propagation import AffinityPropagation
+import matplotlib.pyplot as plt
+from itertools import cycle
 
 
 def generate_random_cluster_data(n=5, m=10) -> np.array:
@@ -28,3 +31,41 @@ def generate_random_cluster_data(n=5, m=10) -> np.array:
         X.append(x)
 
     return np.array(X).reshape((-1, 2))
+
+
+def visualize_clusters(clusterer: AffinityPropagation, x: np.ndarray) -> None:
+    """
+    Visualize clusters and their exemplars by saving a figure
+    to the current directory.
+
+    Parameters
+    ---------
+        clusterer : affinity_propagation.AffinityPropagation
+            Clusterer object.
+        x : np.array
+            Cluster data.
+    """
+    print(len(clusterer.exemplars_))
+    colors = dict(zip(clusterer.exemplars_, cycle("bgrcmyk")))
+    for i in range(len(clusterer.labels_)):
+        X = x[i][0]
+        Y = x[i][1]
+
+        if i in clusterer.exemplars_:
+            exemplar = i
+            edge = "k"
+            ms = 10
+
+        else:
+            exemplar = clusterer.labels_[i]
+            ms = 3
+            edge = None
+            plt.plot(
+                [X, x[exemplar][0]], [Y, x[exemplar][1]], c=colors[exemplar]
+            )
+
+        plt.plot(
+            X, Y, "o", markersize=ms, markeredgecolor=edge, c=colors[exemplar]
+        )
+
+    plt.savefig("plot.png")
